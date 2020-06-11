2b013d8dd68a10079c97387f6f9c8d8e2e1a54ce
Michael Puskas <mmpuskas@gmail.com>
2020-06-10 22:57:53 -0700

    Fixed sdl_image link issue, client/server runs on mingw now.

--- CMake/SDL2_image.cmake
+++ CMake/SDL2_image.cmake
@@ -29,26 +29,31 @@ target_compile_definitions(SDL2_image-static PRIVATE
 		-DLOAD_TGA -DLOAD_XCF -DLOAD_XPM -DLOAD_XV -DLOAD_XPM)
 
 if (SUPPORT_JPG)
-	target_compile_definitions(SDL2_image PRIVATE -DLOAD_JPG)
-	add_subdirectory(${PROJECT_SOURCE_DIR}/../Libraries/SDL_image/external/jpeg-9c)
-	target_link_libraries(SDL2_image PRIVATE jpeg)
+	target_compile_definitions(SDL2_image-static PRIVATE -DLOAD_JPG)
+	add_subdirectory("${PROJECT_SOURCE_DIR}/../Libraries/SDL_image/external/jpeg-9c"
+	                 "${PROJECT_BINARY_DIR}/Libraries/SDL_image/external/jpeg-9c")
+	target_link_libraries(SDL2_image-static PRIVATE jpeg)
 endif()
 
 if (SUPPORT_PNG)
 	# missing libpng.vers
 	set(HAVE_LD_VERSION_SCRIPT OFF CACHE BOOL "" FORCE)
-	target_compile_definitions(SDL2_image PRIVATE -DLOAD_PNG)
+	target_compile_definitions(SDL2_image-static PRIVATE -DLOAD_PNG)
 
-	add_subdirectory(${PROJECT_SOURCE_DIR}/../Libraries/SDL_image/external/libpng-1.6.37)
+    set(PNG_SHARED OFF CACHE BOOL "libpng build shared lib.")
+    set(PNG_STATIC ON CACHE BOOL "libpng build static lib.")
+	add_subdirectory("${PROJECT_SOURCE_DIR}/../Libraries/SDL_image/external/libpng-1.6.37"
+	                 "${PROJECT_BINARY_DIR}/Libraries/SDL_image/external/libpng-1.6.37")
 	include_directories(${PROJECT_SOURCE_DIR}/../Libraries/SDL_image/external/libpng-1.6.37)
-	target_link_libraries(SDL2_image PRIVATE png)
+	target_link_libraries(SDL2_image-static PRIVATE png_static)
 endif()
 
 if (SUPPORT_WEBP)
-	target_compile_definitions(SDL2_image PRIVATE -DLOAD_WEBP)
+	target_compile_definitions(SDL2_image-static PRIVATE -DLOAD_WEBP)
 	# missing cpufeatures
-	add_subdirectory(${PROJECT_SOURCE_DIR}/../Libraries/SDL_image/external/libwebp-1.0.3)
-	target_link_libraries(SDL2_image PRIVATE webp)
+	add_subdirectory("${PROJECT_SOURCE_DIR}/../Libraries/SDL_image/external/libwebp-1.0.3"
+	                 "${PROJECT_BINARY_DIR}/Libraries/SDL_image/external/libwebp-1.0.3")
+	target_link_libraries(SDL2_image-static PRIVATE webp)
 endif()
 
 target_include_directories(SDL2_image-static
