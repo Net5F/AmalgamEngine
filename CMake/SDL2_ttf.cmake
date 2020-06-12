e0aeb77a3e28219a928bd283919d2e63ae852f3b
Michael Puskas <mmpuskas@gmail.com>
2020-06-12 09:56:30 -0700

    Revert "Build freetype and link as static, removed dynamic libs."
    This reverts commit 6c774347249b25c23f0450c49d42d73d7a6a9131.

--- CMake/SDL2_ttf.cmake
+++ CMake/SDL2_ttf.cmake
@@ -2,16 +2,14 @@ cmake_minimum_required(VERSION 3.5)
 
 message(STATUS "Configuring SDL_ttf")
 
+find_package(Freetype REQUIRED)
+
 # Add our static library target
 add_library(SDL2_ttf-static STATIC
        ${PROJECT_SOURCE_DIR}/../Libraries/SDL_ttf/SDL_ttf.c
        ${PROJECT_SOURCE_DIR}/../Libraries/SDL_ttf/SDL_ttf.h
 )
 set_target_properties(SDL2_ttf-static PROPERTIES OUTPUT_NAME "SDL2_ttf")
-
-# Build Freetype before linking.
-add_subdirectory("${PROJECT_SOURCE_DIR}/../Libraries/SDL_ttf/external/freetype-2.10.1"
-                 "${PROJECT_BINARY_DIR}/Libraries/SDL_ttf/external/freetype-2.10.1")
 
 target_include_directories(SDL2_ttf-static
     PUBLIC
@@ -21,5 +19,5 @@ target_include_directories(SDL2_ttf-static
 target_link_libraries(SDL2_ttf-static
     PRIVATE
         SDL2-static
-        freetype
+        Freetype::Freetype
 )
