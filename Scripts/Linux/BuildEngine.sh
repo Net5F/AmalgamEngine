4ea10d5b5ba73c8910949273ce981f0527ac85fb
Michael Puskas <mmpuskas@gmail.com>
2020-06-15 15:44:27 -0700

    Updated BuildEngine to be callable from anywhere.

--- Scripts/Linux/BuildEngine.sh
+++ Scripts/Linux/BuildEngine.sh
@@ -4,33 +4,31 @@ if [[ $# -eq 0 ]] ; then
     echo "Builds the engine targets selected in Source/CMakeLists.txt"
     echo "Usage: ./BuildEngine <Debug/Release> <(Optional) Clobber>"
     echo "Clobber as the second argument will clear the build directory before building."
-    echo "Uses relative paths, so make sure to run this from Scripts/Linux"
     exit 1
 fi
 
-# Check if we're in the right directory.
-if ! [ -f "BuildEngine.sh" ] ; then
-    echo "BuildEngine.sh was not found in the current directory. Please run from Scripts/Linux."
-fi
+# Grab the full path to the base of the repo (assuming this script is in Scripts/Linux)
+BasePath="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." >/dev/null 2>&1 && pwd )"
 
-# Get into the right directory.
+# Make the directory we're going to use.
 Config=$1
-cd ../..
-mkdir -p Build/Linux/$Config
-cd Build/Linux/$Config
+BuildPath=$BasePath/Build/Linux/$Config
+mkdir -p $BuildPath
 
+# If a clobber was requested, clobber the old files.
 if [ $# -eq 2 ] && [ $2 = "Clobber" ] ; then
     # Check if we're in the right directory.
-    if ! [ -f "CMakeCache.txt" ] ; then
+    if ! [ -f "$BuildPath/CMakeCache.txt" ] ; then
         echo "Clobber requested, but CMakeCache.txt was not found in the current directory. Aborting for safety."
         exit 1
     fi
     
-    rm -r ./*
-    echo "Clobbered the directory."
+    rm -r $BuildPath/*
+    echo "Clobbered $BuildPath."
 fi
 
 echo "Starting build."
-cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -G Ninja ../../../Source/
+cd $BuildPath
+cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -G Ninja $BasePath/Source/
 ninja all
     
