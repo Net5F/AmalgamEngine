5c7a0a11be9e137b25bb57b328f071e0b6ff08c9
Michael Puskas <mmpuskas@gmail.com>
2020-06-29 20:25:31 -0700

    Started building latency test.

---
+++ Source/Tests/NetworkTests/Private/LatencyTestClientMain.cpp
@@ -0,0 +1,9 @@
+#include "SDL.h"
+#include "SDL_net.h"
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Client" << std::endl;
+    return 0;
+}
---
+++ Source/Tests/NetworkTests/Private/LatencyTestServerMain.cpp
@@ -0,0 +1,9 @@
+#include "SDL.h"
+#include "SDL_net.h"
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Server" << std::endl;
+    return 0;
+}
