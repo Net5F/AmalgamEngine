e4f2a35f3052ee16c0b16ee79b47a8d0c7c1cfd9
Michael Puskas <mmpuskas@gmail.com>
2022-03-20 12:34:08 -0700

    Added AM_Assert() for later use.

---
+++ Source/Shared/Utility/Public/AMAssert.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Log.h"
+
+/**
+ * Assert macro. Use this in place of assert().
+ */
+#ifndef NDEBUG
+#define AM_ASSERT(condition, ...) \
+    do { \
+        if (!(condition)) { \
+            LOG_ERROR(__VA_ARGS__); \
+        } \
+    } while (false)
+#else
+#define AM_ASSERT(condition, ...) do { } while (false)
+#endif
