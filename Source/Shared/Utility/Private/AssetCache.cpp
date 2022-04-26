09925f1644ae8a705b7334219d7d8fe3c11f388f
Michael Puskas <mmpuskas@gmail.com>
2022-04-25 21:58:59 -0700

    Minor code style changes.

--- Source/Shared/Utility/Private/AssetCache.cpp
+++ Source/Shared/Utility/Private/AssetCache.cpp
@@ -11,20 +11,20 @@ AssetCache::AssetCache(SDL_Renderer* inSdlRenderer)
 TextureHandle AssetCache::loadTexture(const std::string& filePath)
 {
     // If the texture is already loaded, return it.
-    auto it = textureCache.find(filePath);
+    auto it{textureCache.find(filePath)};
     if (it != textureCache.end()) {
         return it->second;
     }
 
     // Load the texture.
-    SDL_Texture* texture = IMG_LoadTexture(sdlRenderer, filePath.c_str());
+    SDL_Texture* texture{IMG_LoadTexture(sdlRenderer, filePath.c_str())};
     if (texture == nullptr) {
         LOG_FATAL("Failed to load texture: %s", filePath.c_str());
     }
 
     // Wrap the texture in a shared_ptr.
-    TextureHandle handle
-        = TextureHandle(texture, [](SDL_Texture* p) { SDL_DestroyTexture(p); });
+    TextureHandle handle{
+        TextureHandle(texture, [](SDL_Texture* p) { SDL_DestroyTexture(p); })};
 
     // Save the texture in the cache.
     textureCache[filePath] = handle;
@@ -35,7 +35,7 @@ TextureHandle AssetCache::loadTexture(const std::string& filePath)
 bool AssetCache::discardTexture(const std::string& filePath)
 {
     // If the cache contains the given texture, discard it.
-    auto it = textureCache.find(filePath);
+    auto it{textureCache.find(filePath)};
     if (it != textureCache.end()) {
         textureCache.erase(it);
         return true;
