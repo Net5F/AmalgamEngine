db6fdac946d93d6b3a3d675a46bcaac43b090df2
Michael Puskas <mmpuskas@gmail.com>
2022-08-21 13:41:45 -0700

    Added default initialization to a couple spots.

--- Source/SharedLib/Simulation/Public/TileMap/Tile.h
+++ Source/SharedLib/Simulation/Public/TileMap/Tile.h
@@ -28,7 +28,7 @@ public:
             moved to match the tile's world position.
             Tiles don't move, so we can calculate this ahead of time and save
             it here. */
-        BoundingBox worldBounds;
+        BoundingBox worldBounds{};
     };
 
     /** The layers of sprites that make up this tile, ordered bottom to top.
@@ -36,7 +36,7 @@ public:
         Sprites with bounding boxes will be rendered in an order corresponding
         to their box extent, but sprites with no box will be rendered by order
         of appearance in this vector, from begin -> end. */
-    std::vector<SpriteLayer> spriteLayers;
+    std::vector<SpriteLayer> spriteLayers{};
 };
 
 } // End namespace AM
