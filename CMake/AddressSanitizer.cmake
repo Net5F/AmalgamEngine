acf1b673b1e5e6d77e4e751130a86c7aba5bee2f
Michael Puskas <mmpuskas@gmail.com>
2022-09-20 09:08:44 -0700

    Changed ASan/TSan logic.
    Changes:
    ASan/TSan are now initiated by the project, so that both the project
    code and engine code are properly sanitized.

--- CMake/AddressSanitizer.cmake
+++ CMake/AddressSanitizer.cmake
@@ -1,30 +1,30 @@
 # Support for building with AddressSanitizer (ASan) -
 # https://code.google.com/p/address-sanitizer/
+#
+# Note: Including this file enables ASan for all targets in the current 
+#       directory and all sub-directories (that are added after including 
+#       this file).
 
 INCLUDE(CheckCCompilerFlag)
 INCLUDE(CheckCXXCompilerFlag)
 INCLUDE(CMakePushCheckState)
 
-OPTION(AM_ADDRESSSANITIZER "Enable AddressSanitizer memory error detector." OFF)
+CMAKE_PUSH_CHECK_STATE(RESET)
+SET(CMAKE_REQUIRED_FLAGS "-fsanitize=address") # Also needs to be a link flag for test to pass
+CHECK_C_COMPILER_FLAG("-fsanitize=address" HAVE_FLAG_SANITIZE_ADDRESS_C)
+CHECK_CXX_COMPILER_FLAG("-fsanitize=address" HAVE_FLAG_SANITIZE_ADDRESS_CXX)
+CMAKE_POP_CHECK_STATE()
 
-IF (AM_ADDRESSSANITIZER)
-    CMAKE_PUSH_CHECK_STATE(RESET)
-    SET(CMAKE_REQUIRED_FLAGS "-fsanitize=address") # Also needs to be a link flag for test to pass
-    CHECK_C_COMPILER_FLAG("-fsanitize=address" HAVE_FLAG_SANITIZE_ADDRESS_C)
-    CHECK_CXX_COMPILER_FLAG("-fsanitize=address" HAVE_FLAG_SANITIZE_ADDRESS_CXX)
-    CMAKE_POP_CHECK_STATE()
+IF(HAVE_FLAG_SANITIZE_ADDRESS_C AND HAVE_FLAG_SANITIZE_ADDRESS_CXX)
+    SET(ADDRESS_SANITIZER_FLAG "-fsanitize=address")
 
-    IF(HAVE_FLAG_SANITIZE_ADDRESS_C AND HAVE_FLAG_SANITIZE_ADDRESS_CXX)
-        SET(ADDRESS_SANITIZER_FLAG "-fsanitize=address")
+    SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ADDRESS_SANITIZER_FLAG}")
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ADDRESS_SANITIZER_FLAG}")
+    SET(CMAKE_CGO_LDFLAGS "${CMAKE_CGO_LDFLAGS} ${ADDRESS_SANITIZER_FLAG}")
 
-        SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ADDRESS_SANITIZER_FLAG}")
-        SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ADDRESS_SANITIZER_FLAG}")
-        SET(CMAKE_CGO_LDFLAGS "${CMAKE_CGO_LDFLAGS} ${ADDRESS_SANITIZER_FLAG}")
+    ADD_DEFINITIONS(-DADDRESS_SANITIZER)
 
-        ADD_DEFINITIONS(-DADDRESS_SANITIZER)
-
-        MESSAGE(STATUS "AM: AddressSanitizer enabled.")
-    ELSE()
-        MESSAGE(FATAL_ERROR "AM_ADDRESSSANITIZER enabled but compiler doesn't support AddressSanitizer - cannot continue.")
-    ENDIF()
+    MESSAGE(STATUS "AM: AddressSanitizer enabled.")
+ELSE()
+    MESSAGE(FATAL_ERROR "AM_ADDRESSSANITIZER enabled but compiler doesn't support AddressSanitizer - cannot continue.")
 ENDIF()
--- CMake/ThreadSanitizer.cmake
+++ CMake/ThreadSanitizer.cmake
@@ -3,31 +3,31 @@
 #
 # Copied from ForestDB under the Apache-2.0 License.
 # https://github.com/couchbase/forestdb
+#
+# Note: Including this file enables TSan for all targets in the current 
+#       directory and all sub-directories (that are added after including 
+#       this file).
 
 INCLUDE(CheckCCompilerFlag)
 INCLUDE(CheckCXXCompilerFlag)
 INCLUDE(CMakePushCheckState)
 
-OPTION(AM_THREADSANITIZER "Enable ThreadSanitizer data race detector." OFF)
+CMAKE_PUSH_CHECK_STATE(RESET)
+SET(CMAKE_REQUIRED_FLAGS "-fsanitize=thread") # Also needs to be a link flag for test to pass
+CHECK_C_COMPILER_FLAG("-fsanitize=thread" HAVE_FLAG_SANITIZE_THREAD_C)
+CHECK_CXX_COMPILER_FLAG("-fsanitize=thread" HAVE_FLAG_SANITIZE_THREAD_CXX)
+CMAKE_POP_CHECK_STATE()
 
-IF (AM_THREADSANITIZER)
-    CMAKE_PUSH_CHECK_STATE(RESET)
-    SET(CMAKE_REQUIRED_FLAGS "-fsanitize=thread") # Also needs to be a link flag for test to pass
-    CHECK_C_COMPILER_FLAG("-fsanitize=thread" HAVE_FLAG_SANITIZE_THREAD_C)
-    CHECK_CXX_COMPILER_FLAG("-fsanitize=thread" HAVE_FLAG_SANITIZE_THREAD_CXX)
-    CMAKE_POP_CHECK_STATE()
+IF(HAVE_FLAG_SANITIZE_THREAD_C AND HAVE_FLAG_SANITIZE_THREAD_CXX)
+    SET(THREAD_SANITIZER_FLAG "-fsanitize=thread")
 
-    IF(HAVE_FLAG_SANITIZE_THREAD_C AND HAVE_FLAG_SANITIZE_THREAD_CXX)
-        SET(THREAD_SANITIZER_FLAG "-fsanitize=thread")
+    SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${THREAD_SANITIZER_FLAG}")
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${THREAD_SANITIZER_FLAG}")
+    SET(CMAKE_CGO_LDFLAGS "${CMAKE_CGO_LDFLAGS} ${THREAD_SANITIZER_FLAG}")
 
-        SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${THREAD_SANITIZER_FLAG}")
-        SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${THREAD_SANITIZER_FLAG}")
-        SET(CMAKE_CGO_LDFLAGS "${CMAKE_CGO_LDFLAGS} ${THREAD_SANITIZER_FLAG}")
+    ADD_DEFINITIONS(-DTHREAD_SANITIZER)
 
-        ADD_DEFINITIONS(-DTHREAD_SANITIZER)
-
-        MESSAGE(STATUS "AM: ThreadSanitizer enabled.")
-    ELSE()
-        MESSAGE(FATAL_ERROR "AM_THREADSANITIZER enabled but compiler doesn't support ThreadSanitizer - cannot continue.")
-    ENDIF()
+    MESSAGE(STATUS "AM: ThreadSanitizer enabled.")
+ELSE()
+    MESSAGE(FATAL_ERROR "AM_THREADSANITIZER enabled but compiler doesn't support ThreadSanitizer - cannot continue.")
 ENDIF()
