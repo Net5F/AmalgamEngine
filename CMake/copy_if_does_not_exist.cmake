113171ad12a25cf066bb0c5670fbb8a53d471085
Net_ <the.net.persists@gmail.com>
2025-10-07 15:08:28 -0700

    Fixed cmake copy script.

--- CMake/copy_if_does_not_exist.cmake
+++ CMake/copy_if_does_not_exist.cmake
@@ -1,19 +1,10 @@
-# Copies a file only if it doesn't already exist at the destination.
+# Copies a file into CMAKE_CURRENT_BINARY_DIR only if it doesn't already exist there.
 function(copy_if_does_not_exist fileRelPath)
     set(inputFilePath "${PROJECT_SOURCE_DIR}/${fileRelPath}")
     set(outputDir "${CMAKE_CURRENT_BINARY_DIR}/")
     get_filename_component(outputFileName ${fileRelPath} NAME)
     set(outputFilePath "${outputDir}${outputFileName}")
-    file(
-        GENERATE 
-            OUTPUT ${outputFilePath}
-            CONTENT 
-                "if (NOT EXISTS ${outputFilePath})
-                    execute_process(
-                         COMMAND \"${CMAKE_COMMAND}\" -E copy
-                             ${inputFilePath}
-                             ${outputDir}
-                    )
-                endif()"
-    )
+    if (NOT EXISTS ${outputFilePath})
+        file(COPY ${inputFilePath} DESTINATION ${outputDir})
+    endif()
 endfunction()
